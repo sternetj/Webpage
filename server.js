42f9f1a057bcf923d7cacd03ed91c331b43673ba
Teddy Sterne <pumpdude93@gmail.com>
2017-05-18 18:53:42 -0400

    setting port from config

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 80;
+
+app.use(express.static(__dirname))
+
+app.listen(port, function () {
+  console.log('App listening on port ' + port + '!')
+});
