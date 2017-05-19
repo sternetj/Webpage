aaaf82cea720240a97fd65d4f16b9d5c3372c925
Teddy Sterne <pumpdude93@gmail.com>
2017-05-19 15:21:56 -0400

    restructured the site and added new content.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 80;
 
-app.use(express.static(__dirname))
+app.use(express.static(__dirname + '/public'))
 
 app.listen(port, function () {
   console.log('App listening on port ' + port + '!')
